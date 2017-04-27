43ba02f1634a593e8d3aff204d80f0710ac18931
bacondrake <frank.pavey@gmail.com>
2017-04-27 10:43:55 +1000

    Level 2, add some styles and tabs, more controllers and directives

--- app.js
+++ app.js
@@ -2,10 +2,26 @@
   // Create the dictionary module
   var app = angular.module('dictionary', []);
 
-  // Create the controller
+  // Create the dictionary controller
   app.controller('DictionaryController', function() {
     // Word is a Property of Dictionary
     this.words = definitions;
+  });
+
+  // Controller for panel logic
+  app.controller('PanelController', function() {
+    this.tab = 1; // initialise tab to 1
+
+    // Triggers when clicking a navigation tab
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    }
+
+    // Check if tab is selected, return that tab value that's selected
+    // In the HTML it will set the active-pill class if the tab is selected
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    }
   });
 
   // Think of this simply as the data
@@ -23,6 +39,20 @@
       sample: "The river was turbid from the pollution of the nearby factory.",
       comment: "Adjective",
       memorised: false
+    },
+    {
+      name: "Abut",
+      meaning: "be next to or have a common boundary with",
+      sample: "The houses in the centre of the town were abutting due to lack of space",
+      comment: "verb",
+      memorised: false
+    },
+    {
+      name: "Pecuniary",
+      meaning: "relating to or consisting of money",
+      sample: "he admitted obtaining a pecuniary advantage by deception.",
+      comment: "adjective formal",
+      memorised: true
     }
   ];
 })();
