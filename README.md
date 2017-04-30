5a462e348267ad21eda212dda867d9f182ea2428
bacondrake <frank.pavey@gmail.com>
2017-04-30 23:51:52 +1000

    Level 4, add directives, clean up index page

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This repo is a reminder of the lessons learned in the [Code School](http://campus.codeschool.com/courses/shaping-up-with-angularjs/level/1/section/1/video/1) Angular course.
 
 Keeping everything basic - no back-end stuff going on, no database or anything. Focus here is on the front-end Angular stuff.
+
+Run locally by running ```http-server .``` from the base directory
--- app.js
+++ app.js
@@ -2,33 +2,68 @@
   // Create the dictionary module
   var app = angular.module('dictionary', []);
 
+  //Controllers//
+
   // Create the dictionary controller
   app.controller('DictionaryController', function() {
     // Word is a Property of Dictionary
     this.words = definitions;
   });
 
-  // Controller for panel logic
-  app.controller('PanelController', function() {
-    this.tab = 1; // initialise tab to 1
+  // app.controller('NewWordController', function() {
+  //   this.newWord = {}; // This handles the form you use to make a new word, stores the word information here temporarily until it's either submitted or removed.
+  //   this.addNewWord = function() {
+  //     definitions.push(this.newWord); // Push the new word to the definitions array.
+  //     this.newWord = {} // Resets the form to be blank after submitting the new word.
+  //   };
+  // });
 
-    // Triggers when clicking a navigation tab
-    this.selectTab = function(setTab) {
-      this.tab = setTab;
-    }
-
-    // Check if tab is selected, return that tab value that's selected
-    // In the HTML it will set the active-pill class if the tab is selected
-    this.isSelected = function(checkTab) {
-      return this.tab === checkTab;
-    }
+  // Directives //
+  // New definition form
+  app.directive('definitionForm', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'definition-form.html',
+      controller: function() {
+        this.newWord = {}; // This handles the form you use to make a new word, stores the word information here temporarily until it's either submitted or removed.
+        this.addNewWord = function() {
+          definitions.push(this.newWord); // Push the new word to the definitions array.
+          this.newWord = {}; // Resets the form to be blank after submitting the new word.
+        };
+      },
+      controllerAs:'newWordCtrl'
+    };
   });
 
-  app.controller('NewWordController', function() {
-    this.newWord = {}; // This handles the form you use to make a new word, stores the word information here temporarily until it's either submitted or removed.
-    this.addNewWord = function() {
-      definitions.push(this.newWord); // Push the new word to the definitions array.
-      this.newWord = {} // Resets the form to be blank after submitting the new word.
+  // Main panel
+  app.directive('definitionPanel', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'definition-panel.html',
+      // Controller for panel logic
+      controller: function() {
+        this.tab = 1; // initialise tab to 1
+
+        // Triggers when clicking a navigation tab
+        this.selectTab = function(setTab) {
+          this.tab = setTab;
+        }
+
+        // Check if tab is selected, return that tab value that's selected
+        // In the HTML it will set the active-pill class if the tab is selected
+        this.isSelected = function(checkTab) {
+          return this.tab === checkTab;
+        }
+      },
+      controllerAs:'panel'
+    };
+  });
+
+  // Definitions
+  app.directive('definitionContent', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'definition.html'
     };
   });
 
